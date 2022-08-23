642511581e30cc2dedd00e46d9a4b7b80d7ebf09
Dogukan Karatas <61163577+dogukankaratas@users.noreply.github.com>
2022-08-23 14:25:11 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const { auto } = require("async");
 module.exports = {
     title: '',
     description: 'Welcome to RFEM Documentation',
-    base: '/',
+    base: "/.github/"
     themeConfig: {
         logo: '/images/dev_docs.png',
         nav: [
